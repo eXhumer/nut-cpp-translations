4fe355e81d4dcfd6af2cdae7cc98be110be7cfcd
eXhumer <exhumer@member.fsf.org>
2021-01-03 00:52:40 -0700

    Update translations for en-US

--- NUT.en_US.ts
+++ NUT.en_US.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="9"/>
         <source>app.header.scan_btn</source>
-        <translation type="unfinished"></translation>
+        <translation>Scan</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="12"/>
         <source>app.header.organize_btn</source>
-        <translation type="unfinished"></translation>
+        <translation>Organize Files</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="15"/>
         <source>app.header.pull_btn</source>
-        <translation type="unfinished"></translation>
+        <translation>Pull</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="18"/>
         <source>app.header.update_titledb_btn</source>
-        <translation type="unfinished"></translation>
+        <translation>Update TitleDB</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="21"/>
         <source>app.header.decompress_nsz_btn</source>
-        <translation type="unfinished"></translation>
+        <translation>Decompress NSZ</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="24"/>
         <source>app.header.compress_nsz_btn</source>
-        <translation type="unfinished"></translation>
+        <translation>Compress NSP</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="27"/>
         <source>app.header.grant_gdrive_btn</source>
-        <translation type="unfinished"></translation>
+        <translation>Setup GDrive OAuth</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="32"/>
         <source>PLACEHOLDER_1</source>
-        <translation type="unfinished"></translation>
+        <translation>Placeholder 1</translation>
     </message>
     <message>
         <location filename="../../appwidgets/headerwidget.cpp" line="36"/>
         <source>PLACEHOLDER_2</source>
-        <translation type="unfinished"></translation>
+        <translation>Placeholder 2</translation>
     </message>
 </context>
 <context>
@@ -54,82 +54,82 @@
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="33"/>
         <source>central_widget.tab.files</source>
-        <translation type="unfinished"></translation>
+        <translation>Files</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="34"/>
         <source>central_widget.tab.filters</source>
-        <translation type="unfinished"></translation>
+        <translation>Filters</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="35"/>
         <source>central_widget.tab.save_paths</source>
-        <translation type="unfinished"></translation>
+        <translation>Save Paths</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="36"/>
         <source>central_widget.tab.local_scan_paths</source>
-        <translation type="unfinished"></translation>
+        <translation>Local Scan Paths</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="37"/>
         <source>central_widget.tab.remote_pull_paths</source>
-        <translation type="unfinished"></translation>
+        <translation>Remote Pull Paths</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="38"/>
         <source>central_widget.tab.users</source>
-        <translation type="unfinished"></translation>
+        <translation>Users</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="39"/>
         <source>central_widget.tab.options</source>
-        <translation type="unfinished"></translation>
+        <translation>Options</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="43"/>
         <source>menu_bar.file</source>
-        <translation type="unfinished"></translation>
+        <translation>File</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="44"/>
         <source>menu_bar.file.exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="46"/>
         <source>menu_bar.help</source>
-        <translation type="unfinished"></translation>
+        <translation>Help</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="47"/>
         <source>menu_bar.help.about</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="49"/>
         <source>about_window_title</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="49"/>
         <source>about_window_text</source>
-        <translation type="unfinished"></translation>
+        <translation>Placeholder About Text</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="51"/>
         <source>menu_bar.help.aboutqt</source>
-        <translation type="unfinished"></translation>
+        <translation>About Qt</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="53"/>
         <source>about_qt_window_title</source>
-        <translation type="unfinished"></translation>
+        <translation>About Qt</translation>
     </message>
     <message>
         <location filename="../../appwidgets/mainwindow.cpp" line="57"/>
         <source>status_bar.ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Ready</translation>
     </message>
 </context>
 <context>
@@ -137,7 +137,7 @@
     <message>
         <location filename="../../appwidgets/progresswidget.cpp" line="13"/>
         <source>0%</source>
-        <translation type="unfinished"></translation>
+        <translation>0%</translation>
     </message>
 </context>
 </TS>
